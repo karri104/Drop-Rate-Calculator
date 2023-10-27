fd19b37950af83ee62e507b22ab1d1f0ab3abbf5
@jarvink7 <karri.jarvinen@aalto.fi>
2023-10-27 15:49:47 +0300

    Switched wording back to what it was before

--- main.py
+++ main.py
@@ -3,9 +3,9 @@ from calculations import calc_probabilities as calc_probs
 
 def main():
 
-    chance = input("Give the occurence chance using format x/y:\n")
-    n = int(input("Give current amount of triggers (events that may lead to a thing happening):\n"))
-    k = int(input("Give the current amount of occurences:\n"))
+    chance = input("Give the drop chance using format x/y:\n")
+    n = int(input("Give current killcount:\n"))
+    k = int(input("Give the current amount of drops:\n"))
 
     prob = chance.split("/")
     prob = [float(x) for x in prob]
@@ -13,23 +13,23 @@ def main():
     chances = calc_probs(prob, n, k)
 
     if k == 0:
-        #Chance to get 0 occurences
-        print(f"The chance of getting 0 occurences is {chances[1]:.2f}%.")
+        #Chance to get 0 drops
+        print(f"The chance of getting 0 drops is {chances[1]:.2f}%.")
 
-        #Chance to get more than 0 occurences
-        print(f"The chance of getting more than 0 occurences is {chances[2]:.2f}%.")
+        #Chance to get more than 0 drops
+        print(f"The chance of getting more than 0 drops is {chances[2]:.2f}%.")
     else:
-        #Chance to get less than x occurences
-        print(f"Chance to get less than {k} occurences is {chances[0]:.2f}%.")
+        #Chance to get less than x drops
+        print(f"Chance to get less than {k} drops is {chances[0]:.2f}%.")
 
-        #Chance to get exactly x occurences
-        print(f"Chance to get exactly {k} occurences is {chances[1]:.2f}%.")
+        #Chance to get exactly x drops
+        print(f"Chance to get exactly {k} drops is {chances[1]:.2f}%.")
 
-        #Chance to get more than x occurences
-        print(f"Chance to get more than {k} occurences is {chances[2]:.2f}%.")
+        #Chance to get more than x drops
+        print(f"Chance to get more than {k} drops is {chances[2]:.2f}%.")
 
-        #Expected amount of occurences
-        print(f"Expected amount of occurences is {n * (prob[0] / prob[1]):.4f}.")
+        #Expected amount of drops
+        print(f"Expected amount of drops is {n * (prob[0] / prob[1]):.4f}.")
 
     input("Press enter to exit.\n")
 
