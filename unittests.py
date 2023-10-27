17ada5c32dc35607bb93c44b61fff6cc4377277c
@jarvink7 <karri.jarvinen@aalto.fi>
2023-10-27 15:58:32 +0300

    Added unittest template. (about time since calculations broke during refactoring :D)

---
+++ unittests.py
@@ -0,0 +1,17 @@
+import unittest
+
+class TestCalculations(unittest.TestCase):
+    def test_calc_fac(self):
+        pass
+
+    def test_pmf(self):
+        pass
+
+    def test_bc(self):
+        pass
+
+    def test_calc_single(self):
+        pass
+
+    def test_calc_probabilities(self):
+        pass
